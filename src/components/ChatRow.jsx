2df09f5c3c1da275858bd981f44fd6be5ce1d21c
webcoderspeed <webcoderspeed@gmail.com>
2023-02-26 18:11:29 +0530

    last message

--- src/components/ChatRow.jsx
+++ src/components/ChatRow.jsx
@@ -3,15 +3,43 @@ import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import useAuth from '../hooks/useAuth';
 import getMatchedUserInfo from '../utils/getMatchedUserInfo';
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from 'firebase/firestore';
+import { db } from '../firebase';
 
 const ChatRow = ({ matchDetails }) => {
   const navigation = useNavigation();
   const { user } = useAuth();
   const [matchedUserInfo, setMatchedUserInfo] = useState(null);
+  const [lastMessage, setLastMessage] = useState(null);
 
   useEffect(() => {
     setMatchedUserInfo(getMatchedUserInfo(matchDetails.users, user.uid));
   }, [matchDetails, user]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, 'matches', matchDetails.id, 'messages'),
+          orderBy('timestamp', 'desc'),
+          limit(1)
+        ),
+        (snapshot) =>
+          setLastMessage(
+            snapshot.docs[0]?.data()?.message
+          )
+      ),
+
+    [matchDetails, db]
+  );
+
+
 
   return (
     <TouchableOpacity
@@ -36,7 +64,7 @@ const ChatRow = ({ matchDetails }) => {
         <Text className='text-lg font-semibold'>
           {matchedUserInfo?.displayName}
         </Text>
-        <Text>Say Hi!</Text>
+        <Text>{lastMessage ?? 'Say Hi!'}</Text>
       </View>
     </TouchableOpacity>
   );
